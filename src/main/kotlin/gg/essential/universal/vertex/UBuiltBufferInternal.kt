be503ddae55bd867ff8fdec8caaf79da4ebc2fe4
Jonas Herzig <jonas@spark-squared.com>
2026-08-17 12:02:56 +0200

    UBuiltBuffer: Add `toByteBuffer`
    To allow one to upload the data to a GpuBuffer.

--- src/main/kotlin/gg/essential/universal/vertex/UBuiltBufferInternal.kt
+++ src/main/kotlin/gg/essential/universal/vertex/UBuiltBufferInternal.kt
@@ -1,5 +1,7 @@
 package gg.essential.universal.vertex
 
+import java.nio.ByteBuffer
+
 //#if STANDALONE
 //$$ import gg.essential.universal.standalone.render.BufferBuilder as BuiltBuffer
 //#elseif MC>=12100
@@ -13,4 +15,24 @@ import net.minecraft.client.renderer.WorldRenderer as BuiltBuffer
 internal interface UBuiltBufferInternal : UBuiltBuffer {
     val mc: BuiltBuffer
     fun closedExternally()
+
+    override fun toByteBuffer(): ByteBuffer {
+        //#if STANDALONE
+        //$$ return mc.byteBuffer
+        //#elseif MC >= 1.21
+        //$$ return mc.buffer
+        //#elseif MC >= 1.19
+        //$$ return mc.vertexBuffer
+        //#elseif MC >= 1.16
+        //$$ return mc.nextBuffer.second
+        //#else
+        return mc.byteBuffer
+        //#endif
+        //#if MC >= 1.20.4 && !STANDALONE
+        //$$     // Buffers used exclusively for sorting won't have any vertex data.
+        //$$     // Our API doesn't provide any way to construct such buffers, so we'll just throw when someone used `wrap`
+        //$$     // on such a buffer.
+        //$$     ?: throw IllegalStateException("Cannot get vertex data of index-only buffer")
+        //#endif
+    }
 }
